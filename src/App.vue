b14703770933b528c0a3923fdee6dac924753b19
Sofya Sevryukova <sonyasevrukova@mail.ru>
2019-05-01 19:06:18 +0300

    Vue initial

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+  <Theme></Theme>
+  </v-app>
+</template>
+
+<script>
+    import {Theme} from './build-entry.js';
+
+    export default {
+        name: 'app',
+        components: {
+            Theme
+        },
+        data() {
+            return {
+                text: ''
+            };
+        }
+    };
+</script>
---
+++ src/build-entry.js
@@ -0,0 +1,12 @@
+import MarkdownEditor from './components/MarkdownEditor.vue';
+import preview from './components/Preview'
+import complex from './components/Theme'
+import marked from './util/marked.js';
+
+export {
+	MarkdownEditor as Editor,
+	preview as Preview,
+	complex as Theme,
+	marked as Renderer
+};
+
